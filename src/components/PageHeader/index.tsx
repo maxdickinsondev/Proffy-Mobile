70795cf568aca4fb3a8720de584133ea23c45aaa
Max Dickinson <maxsantistadickinson@gmail.com>
2020-08-09 18:25:43 -0300

    Criando filtro de professores.

--- src/components/PageHeader/index.tsx
+++ src/components/PageHeader/index.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { ReactNode } from 'react';
 import { View, Image, Text } from 'react-native';
 import { useNavigation } from '@react-navigation/native';
 import { BorderlessButton } from 'react-native-gesture-handler';
@@ -10,9 +10,10 @@ import styles from './styles';
 
 interface PageHeaderProps {
     title: string;
+    headerRight?: ReactNode;
 }
 
-const PageHeader: React.FC<PageHeaderProps> = ({ title }) => {
+const PageHeader: React.FC<PageHeaderProps> = ({ title, children, headerRight }) => {
     const navigation = useNavigation();
 
     function handleGoBack() {
@@ -32,7 +33,12 @@ const PageHeader: React.FC<PageHeaderProps> = ({ title }) => {
                 />
             </View>
 
-            <Text style={styles.title}>{title}</Text>
+            <View style={styles.header}>
+                <Text style={styles.title}>{title}</Text>
+                {headerRight}
+            </View>
+
+            {children}
         </View>
     );
 }
